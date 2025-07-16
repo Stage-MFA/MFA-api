0035d639395e3ce775801495e874c7a593fca68b
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2025-07-16 10:16:10 +0200

    feat: Configuration cors

--- src/main/java/com/school/security/controllers/auth/AuthController.java
+++ src/main/java/com/school/security/controllers/auth/AuthController.java
@@ -5,22 +5,19 @@ import com.school.security.dtos.requests.RefreshReqDto;
 import com.school.security.dtos.requests.UserReqDto;
 import com.school.security.dtos.responses.LoginResDto;
 import com.school.security.dtos.responses.UserResDto;
-import com.school.security.entities.User;
 import com.school.security.securities.services.JwtService;
 import com.school.security.services.contracts.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
 @RestController
 @Slf4j
 @RequestMapping("/auth")
+@CrossOrigin(origins = {"http://localhost:3000", "http://192.168.1.133:3000/"})
 
 public class AuthController {
 
--- src/main/java/com/school/security/securities/config/SecurityConfig.java
+++ src/main/java/com/school/security/securities/config/SecurityConfig.java
@@ -31,7 +31,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
